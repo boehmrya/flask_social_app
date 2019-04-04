176b4639d658834ebfc55b7791a63c0ad16cb5bd
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-04-03 21:44:38 -0400

    set up email functionality for app

--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_script import Shell
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -24,7 +25,17 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 manager.add_command('db', MigrateCommand)
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+
+
 
 
 class Role(db.Model):
