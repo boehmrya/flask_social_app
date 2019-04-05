adfae7a5f3c4afefde2c5e83f06f56eac74c984f
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-04-04 22:21:42 -0400

    implemented email functionality

--- hello.py
+++ hello.py
@@ -10,7 +10,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_script import Shell
 from flask_migrate import Migrate, MigrateCommand
-from flask_mail import Mail
+from flask_mail import Mail, Message
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,6 +20,13 @@ app.config['SECRET_KEY'] = '6+?!FjG2+e;XM(~Ktu"92@*7kdQ8sM%p'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
 
 db = SQLAlchemy(app)
 manager = Manager(app)
@@ -27,15 +35,6 @@ moment = Moment(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 manager.add_command('db', MigrateCommand)
-
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-
-
-
 
 
 class Role(db.Model):
@@ -61,6 +60,14 @@ class NameForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -70,13 +77,15 @@ def index():
             user = User(username = form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                            'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html',
-            form=form, name=session.get('name'),
+    return render_template('index.html', form=form, name=session.get('name'),
             known = session.get('known', False))
 
 
