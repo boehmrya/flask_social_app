6cdbabafaf8bf9d19cf33859e9fafc021c08ea94
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-04-05 21:39:29 -0400

    set up application factory and blueprint

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = '6+?!FjG2+e;XM(~Ktu"92@*7kdQ8sM%p'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
